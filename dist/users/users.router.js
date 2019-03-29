ea4de6f27c2f8a17e8c9013ce05fa5bd3a2e30c1
Eduardo Alessi <eduatletico@gmail.com>
2019-03-29 06:31:43 -0400

    first commit

---
+++ dist/users/users.router.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const model_router_1 = require("../common/model-router");
+const restify = require("restify");
+const users_model_1 = require("./users.model");
+class UsersRouter extends model_router_1.ModelRouter {
+    constructor() {
+        super(users_model_1.User);
+        this.findByEmail = (req, resp, next) => {
+            if (req.query.email) {
+                users_model_1.User.findByEmail(req.query.email)
+                    .then(user => user ? [user] : [])
+                    .then(this.renderAll(resp, next, {
+                    pageSize: this.pageSize,
+                    url: req.url
+                }))
+                    .catch(next);
+            }
+            else {
+                next();
+            }
+        };
+        this.on('beforeRender', document => {
+            document.password = undefined;
+        });
+    }
+    applyRoutes(application) {
+        // application.get({path: '/users', version: '2.0.0'}, [this.findByEmail, this.findAll])
+        // application.get({path: '/users', version: '1.0.0'}, this.findAll)
+        application.get(`${this.basePath}`, restify.plugins.conditionalHandler([
+            { version: '1.0.0', handler: this.findAll },
+            { version: '2.0.0', handler: [this.findByEmail, this.findAll] }
+        ]));
+        application.get(`${this.basePath}/:id`, [this.validateId, this.findById]);
+        application.post(`${this.basePath}`, this.save);
+        application.put(`${this.basePath}/:id`, [this.validateId, this.replace]);
+        application.patch(`${this.basePath}/:id`, [this.validateId, this.update]);
+        application.del(`${this.basePath}/:id`, [this.validateId, this.delete]);
+    }
+}
+exports.usersRouter = new UsersRouter();
+//# sourceMappingURL=users.router.js.map
